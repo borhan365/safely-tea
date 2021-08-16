f1055fcab54b0397405996a0dcca8049e0f593e4
borhan365 <info.mdborhan@gmail.com>
2021-08-17 04:13:11 +0600

    starter

---
+++ js/app.js
@@ -0,0 +1,41 @@
+// Animate items
+new WOW().init();
+
+// Desktop sidebar
+const menuBtn = document.getElementById('menuBtn'); 
+const sidebar = document.getElementById('sidebar');
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.add('active');
+
+})
+
+const cancelBtn = document.getElementById("cancelBtn"); 
+cancelBtn.addEventListener('click', () => {
+  sidebar.classList.remove('active');
+})
+
+
+// Swiper slider home page product slider
+var ProductSlider = new Swiper(".productSlider", {
+    slidesPerView: 2,
+    spaceBetween: 5,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+        640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        },
+        767: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+        },
+        1024: {
+        slidesPerView: 4,
+        spaceBetween: 0,
+        },
+    }
+  });
